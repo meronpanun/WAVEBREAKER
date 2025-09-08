f7b712d5b970c8c0ce93b787043c59355d3938ec
meronpanun <fko2447021@stu.o-hara.ac.jp>
2025-09-08 15:33:13 +0900

    up

---
+++ WAVEBREAKER/WAVEBREAKER/game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Game
+{
+	// ‰æ–Êî•ñ‚ğ’è”’è‹`
+//	constexpr int kScreenWidth = 1280;
+//	constexpr int kScreenHeigth = 720;
+	constexpr int kScreenWidth = 640;
+	constexpr int kScreenHeigth = 480;
+	constexpr int kColorBitNum = 32;
+
+	constexpr bool kDefaultWindowMode = true;
+}
